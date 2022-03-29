45b706dc7e66d6d0f6e205433194402ad4f7bc65
Ryan Garner <rpgarner22@gmail.com>
2022-03-28 21:55:59 -0600

    ready to sead reviews, need help with id association

--- models/review.js
+++ models/review.js
@@ -4,7 +4,8 @@ const Review = new Schema(
   {
    comment: { type: String, required: true },
    rating: { type: String, required: true },
-   creater: { type: String, required: true }
+   creater: { type: String, required: true },
+   workout: { type: Schema.ObjectId, ref: 'workouts'}
   },
   { timestamps: true }
 )
---
+++ seed/reviews.js
@@ -0,0 +1,24 @@
+/////////////// IMPORT /////////////////////
+
+const db = require("../db");
+const { Workout, Review } = require("../models");
+
+// Connect to the database
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+/////////////// Workout seed /////////////////////
+const main = async () => {
+   
+  const reviews = [
+  
+  ];
+
+  await Review.insertMany(reviews);
+  console.log("Created some reviews for workouts");
+};
+const run = async () => {
+  await main();
+  db.close();
+};
+
+run();
