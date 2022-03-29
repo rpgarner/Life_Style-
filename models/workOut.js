6497c52494115242baab43b72b1d6378313e99e1
Ryan Garner <rpgarner22@gmail.com>
2022-03-28 21:05:17 -0600

    successfully seeded workouts

--- models/workOut.js
+++ models/workOut.js
@@ -4,8 +4,7 @@ const Workout = new Schema(
   {
     type: { type: String, required: true },
     duration: { type: Number, required: true },
-    description: { type: String, required: true },
-    review: { type: Schema.Types.ObjectId, ref: 'reviews' }
+    description: { type: String, required: true }
   },
   { timestamps: true }
 )
--- seed/workOuts.js
+++ seed/workOuts.js
@@ -1,7 +1,7 @@
 /////////////// IMPORT /////////////////////
 
 const db = require("../db");
-const { WorkOut } = require("../models");
+const { Workout } = require("../models");
 
 // Connect to the database
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -11,12 +11,39 @@ const main = async () => {
    
   const workouts = [
     {
-    
+        type: 'AMRAP (complete as many rounds as possible)',
+        duration: 20,
+        description: '5 strict chest to bar pull ups, 10 ring push ups, 20 box step ups ',
+        
     },
+    {
+        type: 'Barbell lunges and double unders',
+        duration: 15,
+        description: '15 barbell lounges, 75 double unders',
+       
+    },
+    {
+        type: 'AMRAP',
+        duration: 30,
+        description: '10 wall ball shots, 10 toes to bars, 10 box jumps',
+       
+    },
+    {
+        type: 'Hang Squat clean 7x3',
+        duration: 15,
+        description: 'Hang squat clean: 3-3-3-3-3-3-3 reps',
+       
+    },
+    {
+        type: 'Run and pull ups',
+        duration: 21,
+        description: 'as many rounds as possible: run 400/200/100 meters (go down in distance after each 7 minutes), max rep strict pull up (run after you break a set)',
+       
+    }
    
   ];
 
-  await WorkOut.insertMany(workouts);
+  await Workout.insertMany(workouts);
   console.log("Created some games/rides!");
 };
 const run = async () => {
