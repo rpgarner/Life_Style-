7141a279ecbb7a79fd066d275a55ad706d3b8fab
Ryan Garner <rpgarner22@gmail.com>
2022-03-28 20:36:02 -0600

    loaded the erd for the models

--- models/review.js
+++ models/review.js
@@ -2,7 +2,9 @@ const { Schema } = require('mongoose')
 
 const Review = new Schema(
   {
-   
+   comment: { type: String, required: true },
+   rating: { type: String, required: true },
+   creater: { type: String, required: true }
   },
   { timestamps: true }
 )
--- models/workOut.js
+++ models/workOut.js
@@ -2,7 +2,10 @@ const { Schema } = require('mongoose')
 
 const Workout = new Schema(
   {
- 
+    type: { type: String, required: true },
+    duration: { type: Number, required: true },
+    description: { type: String, required: true },
+    review: { type: Schema.Types.ObjectId, ref: 'reviews' }
   },
   { timestamps: true }
 )
