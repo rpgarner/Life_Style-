432c1f3603a261a8866bc7c9af040eae6ce819e9
Ryan Garner <rpgarner22@gmail.com>
2022-03-31 15:33:39 -0600

    tried to get routes and controllers working

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,65 @@
-const { Workout } = require('../models')
+const { Workout, Review } = require("../models/index");
 
+const addWorkout = async (req, res) => {
+  const newWorkout = await Workout.create(req.body);
+  await res.json(newWorkout);
+};
+// read
+// gets workouts from api
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find();
-    res.json(workouts);
-  }
+  const workouts = await Workout.find({});
+  res.json(workouts);
+};
+
+const getReviews = async (req, res) => {
+  const reviews = await Review.find({});
+  res.send(reviews);
+};
+
+const getWorkoutsForComment = async (req, res) => {
+  const workouts = await Workout.find({});
+  res.json(workouts);
+};
+// get workouts by ID to link with reviews
+const getWorkoutbyID = async (req, res) => {
+  const { id } = req.params;
+  let foundWorkout = await Workout.findById(id).populate("reviews");
+  res.json(foundWorkout);
+};
+
+
+// Update
+// put reviews in workout
+const postAComment = async (req, res) => {
+  const { id } = req.params;
+  let foundWorkout = await Workout.findById(id);
+  let createdReview = await Review.create(req.body);
+  foundWorkout.reviews.push(createdReview);
+  foundWorkout.save();
+  res.json(foundWorkout.populate("reviews"));
+};
+//////////update workout/////////
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+  let foundWorkout = await Workout.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.send(foundWorkout);
+};
+
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+  let deletedWorkout = await Workout.findByIdAndDelete(id);
+  res.status(200).send(deletedWorkout);
+};
 
 module.exports = {
-    getWorkouts
-}
+  getWorkouts,
+  addWorkout,
+  getReviews,
+  getWorkoutsForComment,
+  getWorkoutbyID,
+  postAComment,
+  updateWorkout,
+  deleteWorkout 
+};
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,22 @@ const { Router } = require('express')
 const controllers = require('../controllers')
 const router = Router()
 
-router.get('/', (req, res) =>  res.send('This is root!'))
+router.post("/YourWorkouts/addWorkout", controllers.addWorkout)
+
 router.get("/yourWorkouts", controllers.getWorkouts)
+
+router.get("/yourReviews", controllers.getReviews)
+
+router.get("/yourWorkouts/addComment", controllers.getWorkoutsForComment)
+
+router.get("/yourWorkouts/:id", controllers.getWorkoutbyID )
+
+router.post("/yourWorkouts/addComment/:id", controllers.postAComment)
+
+router.put("/yourWorkouts/:id", controllers.updateWorkout)
+
+router.delete("/yourWorkouts/:id", controllers.deleteWorkout)
+
+
 
 module.exports = router;
