e82d26168f94ccce70ccf19706fb8dfa8fbb7f76
Ryan Garner <rpgarner22@gmail.com>
2022-03-31 18:09:19 -0600

    got routes and controllers working

--- models/workOut.js
+++ models/workOut.js
@@ -5,7 +5,7 @@ const Workout = new Schema(
     type: { type: String, required: true },
     duration: { type: String, required: true },
     description: { type: String, required: true },
-    reviews: [{ type: Schema.Types.ObjectId, ref: 'reviews_id'}]
+    reviews: [{ type: Schema.Types.ObjectId, ref: 'review'}]
   },
   { timestamps: true }
 )
--- server.js
+++ server.js
@@ -22,62 +22,78 @@ app.use(bodyParser.json());
 
 /////////// ROUTES //////////////////////
 
-// Create
-//creates wrokout to api
-app.post("/YourWorkouts/addWorkout", async (req, res) => {
-  const newWorkout = await Workout.create(req.body);
-  await res.json(newWorkout);
-});
-// read
-// gets workouts from api
-app.get("/yourWorkouts", async (req, res) => {
-  const workouts = await Workout.find({});
-  res.json(workouts);
-});
+///////////////// Create /////////////
+    //////creates wrokout to api///////
+// app.post("/YourWorkouts/addWorkout", async (req, res) => {
+//   const newWorkout = await Workout.create(req.body);
+//   await res.json(newWorkout);
+// });
+//     ///////create review to api//////
+//  app.post("/yourWorks/addComment", async (req, res) => {
+//    try {
+//      const post = await new Review.findById(req.body)
+//      await post.save()
+//      return res.status(201).json(post)
+//    } catch(err) {
+//      return res.status(500).json(err)
+//    }
 
-app.get("/yourReviews", async (req, res) => {
-  const reviews = await Review.find({});
-  res.send(reviews);
-});
+//  })
 
-app.get("/yourWorkouts/addComment", async (req, res) => {
-  const workouts = await Workout.find({});
-  res.json(workouts);
-});
-// get workouts by ID to link with reviews
-app.get("/yourWorkouts/:id", async (req, res) => {
-  const { id } = req.params;
-  let foundWorkout = await Workout.findById(id).populate("reviews");
-  res.json(foundWorkout);
-});
 
-// app.get("/yourWorkouts/")
-// Update
-// put reviews in workout
-app.post("/yourWorkouts/addComment/:id", async (req, res) => {
-  const { id } = req.params;
-  let foundWorkout = await Workout.findById(id);
-  let createdReview = await Review.create(req.body);
-  foundWorkout.reviews.push(createdReview);
-  foundWorkout.save();
-  res.json(foundWorkout.populate("reviews"));
-});
-//////////update workout/////////
-app.put("/yourWorkouts/:id", async (req, res) => {
-  const { id } = req.params;
-  let foundWorkout = await Workout.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
-  res.send(foundWorkout);
-});
+// // app.post("/yourWorkouts/addComment/:id", async (req, res) => {
+  
+// //   const { id } = req.params;
+// //   let foundWorkout = await Workout.findById(id);
+// //   let createdReview = await Review.create(req.body);
+// //   foundWorkout.reviews.push(createdReview);
+// //   foundWorkout.save();
+// //   res.json(foundWorkout);
+// // });
 
-app.delete("/yourWorkouts/:id", async (req, res) => {
-  const { id } = req.params;
-  let deletedWorkout = await Workout.findByIdAndDelete(id);
-  res.status(200).send(deletedWorkout);
-});
+// ///////////////// read  /////////////////
+//     ////// gets workouts from api ///////
+// app.get("/yourWorkouts", async (req, res) => {
+//   const workouts = await Workout.find({});
+//   res.json(workouts);
+// });
 
-// app.use('/api', routes);
+// app.get("/yourWorkouts", async (req, res) => {
+//   const reviews = await Review.find({});
+//   res.send(reviews);
+// });
+
+// app.get("/yourWorkouts/addComment", async (req, res) => {
+//   const workouts = await Workout.find({});
+//   res.json(workouts);
+// });
+// // get workouts by ID to link with reviews
+// app.get("/yourWorkouts/:id", async (req, res) => {
+//   const { id } = req.params;
+//   let foundWorkout = await Workout.findById(id).populate("reviews");
+//   res.json(foundWorkout);
+// });
+
+// // app.get("/yourWorkouts/")
+// // Update
+// // put reviews in workout
+
+// //////////update workout/////////
+// app.put("/yourWorkouts/:id", async (req, res) => {
+//   const { id } = req.params;
+//   let foundWorkout = await Workout.findByIdAndUpdate(id, req.body, {
+//     new: true,
+//   });
+//   res.send(foundWorkout);
+// });
+
+// app.delete("/yourWorkouts/:id", async (req, res) => {
+//   const { id } = req.params;
+//   let deletedWorkout = await Workout.findByIdAndDelete(id);
+//   res.status(200).send(deletedWorkout);
+// });
+
+app.use('/', routes);
 
 app.get("/*", (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`);
