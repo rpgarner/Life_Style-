bc4fe0d6ce2fb03e9f1b05ab1da7dc97aeffe438
Ryan Garner <rpgarner22@gmail.com>
2022-04-02 11:16:41 -0600

    test

--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,12 @@ const { Workout, Review, Diet } = require("../models");
 ////////////////////////Create////////////////
         /////// Workout //////////
 const addWorkout = async (req, res) => {
-  const newWorkout = await Workout.create(req.body);
+  let newWorkout = await Workout.create(req.body);
   await res.json(newWorkout);
 };
         ////////Review/Comment//////////
 const postAComment = async (req, res) => {
-  const { id } = req.params;
+  let { id } = req.params;
   let foundWorkout = await Workout.findById(id);
   let createdReview = await Review.create(req.body);
   foundWorkout.reviews.push(createdReview);
@@ -19,33 +19,33 @@ const postAComment = async (req, res) => {
 ///////////////// read //////////////////////////
         ///// get workouts ///////
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({});
+  let workouts = await Workout.find({});
   res.json(workouts);
 };
         //////// get reviews //////
 const getReviews = async (req, res) => {
-  const getreviews = await Review.find({});
+  let getreviews = await Review.find({});
   res.json(getreviews);
 };
         ////////  get diets ////////
 const getDiets = async (req, res) => {
-  const diets = await Diet.find({});
+  let diets = await Diet.find({});
   res.json(diets);
 };
         //////// get workouts for comments ///////
 const getWorkoutsForComment = async (req, res) => {
-  const workouts = await Workout.find({});
+  let workouts = await Workout.find({});
   res.json(workouts);
 };
         /////// get workout by ID to populate ////////
 const getWorkoutbyID = async (req, res) => {
-  const { id } = req.params;
+  let { id } = req.params;
   let foundWorkout = await Workout.findById(id).populate("reviews");
   res.json(foundWorkout);
 };
          /////// get review by ID ////////  
 const getReviewsById = async (req, res) => {
-    const { id } = req.params;
+    let { id } = req.params;
     let reviewById = await Review.findById(id)
     res.json(reviewById)
 };
@@ -54,7 +54,7 @@ const getReviewsById = async (req, res) => {
 
     ////////// put workout/////////
 const updateWorkout = async (req, res) => {
-  const { id } = req.params;
+  let { id } = req.params;
   let foundWorkout = await Workout.findByIdAndUpdate(id, req.body, {
     new: true,
   });
@@ -62,7 +62,7 @@ const updateWorkout = async (req, res) => {
 };
 //////////////////// Delete //////////////////////
 const deleteWorkout = async (req, res) => {
-  const { id } = req.params;
+  let { id } = req.params;
   let deletedWorkout = await Workout.findByIdAndDelete(id);
   res.status(200).send(deletedWorkout);
 };
